b01ad1b9387bc4a4f45a2cb65fa4b1cf22e262fb
XceligentITDept <itdept@xceligent.com>
2016-01-15 11:22:08 -0600

    updates
    updates

--- dc.sh
+++ dc.sh
@@ -1,5 +1,5 @@
 #/shell/bash
 sudo wget --no-check-certificate https://github.com/XceligentITDept/DesktopCentral/raw/master/DesktopCentral_LinuxAgent.bin
 sudo wget --no-check-certificate https://github.com/XceligentITDept/DesktopCentral/raw/master/serverinfo.json
-sudo chmod +x /DesktopCentral/DesktopCentral_LinuxAgent.bin
-sudo ./DesktopCentral/DesktopCentral_LinuxAgent.bin
+sudo chmod +x DesktopCentral_LinuxAgent.bin
+sudo ./DesktopCentral_LinuxAgent.bin
